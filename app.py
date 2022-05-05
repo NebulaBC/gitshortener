705cdf645c0ada24f12908e4f422f7eba584efd0
NebulaBC <contact@nebulabc.net>
2022-05-05 17:24:19 -0400

    Update src

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import sqlite3
 from flask import Flask, render_template
-app = Flask(__name__,
-            static_url_path='', 
-            static_folder='web/static',
-            template_folder='web/templates')
 
-@app.route('/')
+app = Flask(
+    __name__,
+    static_url_path="",
+    static_folder="web/static",
+    template_folder="web/templates",
+)
+
+
+@app.route("/")
 def index():
-   return render_template('/index.html')
-if __name__ == '__main__':
-   app.run()
+    return render_template("/index.html")
 
-@app.errorhandler(404) 
-def invalid_route(e): 
-    return render_template('/404.html')
+
+if __name__ == "__main__":
+    app.run()
+
+
+@app.errorhandler(404)
+def invalid_route(e):
+    return render_template("/404.html")
--- readme.rst
+++ readme.rst
@@ -1,5 +1,7 @@
 Source code for neb.cx Git-Shortener
 -------------
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+    :target: https://github.com/psf/black
 
 Git-Shortener is a github/gitlab link shortener. I created this service around
 2/22, but accedentily deleted it. This is a re-write.
