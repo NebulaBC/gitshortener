bcdb6c357a6243e3f8e83e43c079009dd0e0f110
NebulaBC <contact@nebulabc.net>
2022-05-05 17:10:49 -0400

    Update src

--- serve.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-   return render_template('html/index.html')
-if __name__ == '__main__':
-   app.run()
