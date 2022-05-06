af819c4662babbdebea874e441fef86f99a6e148
NebulaBC <contact@nebulabc.net>
2022-05-05 22:54:25 -0400

    Clean up 404/CSS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sqlite3
+from waitress import serve
 from flask import Flask, render_template
 
 app = Flask(
@@ -13,9 +14,14 @@ app = Flask(
 def index():
     return render_template("/index.html")
 
+# @app.route("/<url>")
+# def reroute(url):
+#     return render_template("/index.html")
+
 
 if __name__ == "__main__":
     app.run()
+    # serve(app, host='0.0.0.0', port=8080)
 
 
 @app.errorhandler(404)
