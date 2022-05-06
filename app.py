c268b080135fa3e36bb4df8a312d1bd9c59a60d6
NebulaBC <contact@nebulabc.net>
2022-05-05 23:19:17 -0400

    Update src

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sqlite3
+import uuid
 from waitress import serve
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(
     __name__,
@@ -9,21 +10,23 @@ app = Flask(
     template_folder="web/templates",
 )
 
+# print(uuid.uuid4())'
+
 
 @app.route("/")
 def index():
     return render_template("/index.html")
 
-# @app.route("/<url>")
-# def reroute(url):
-#     return render_template("/index.html")
-
-
-if __name__ == "__main__":
-    app.run()
-    # serve(app, host='0.0.0.0', port=8080)
-
 
 @app.errorhandler(404)
 def invalid_route(e):
     return render_template("/404.html")
+
+
+# @app.route("/git/<url>")
+# def reroute(url):
+#     return render_template("/index.html")
+
+if __name__ == "__main__":
+    app.run()
+    # serve(app, host='0.0.0.0', port=8080)
