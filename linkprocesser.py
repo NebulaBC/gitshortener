40d0c48666b9b01885a5c17e45c44b6256b8d292
NebulaBC <contact@nebulabc.net>
2022-05-24 11:08:16 -0400

    re-black

--- linkprocesser.py
+++ linkprocesser.py
@@ -2,16 +2,24 @@ import os
 import subprocess
 from urllib.parse import urlparse
 
+
 def processurl(url):
     domain = urlparse(url).netloc
     if domain == "":
         print("Please input a valid URL!")
     elif domain == "github.com":
-        shortlink = str(subprocess.check_output("basename " + url.rsplit('.git', 1)[0], shell=True)).rstrip("\\n'").lstrip("b\'")
+        shortlink = (
+            str(
+                subprocess.check_output(
+                    "basename " + url.rsplit(".git", 1)[0], shell=True
+                )
+            )
+            .rstrip("\\n'")
+            .lstrip("b'")
+        )
         if len(shortlink) <= 100:
             print("https://g.neb.cx/" + shortlink)
         else:
             print("Your repo is over 100chars!")
     else:
         print("Please input a github/lab domain!")
-
